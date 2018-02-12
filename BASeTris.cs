525f82fb93923894b86c92c821f35a0bf276c04b
Michael Burgwin <bc_programming@hotmail.com>
2018-02-12 14:20:20 -0800

    Tweak scaling factors

--- BASeTris.cs
+++ BASeTris.cs
@@ -356,13 +356,13 @@ namespace BASeTris
 
         private void xToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            SetScale(1.5f);
+            SetScale(1.3f);
             ((ToolStripMenuItem)sender).Checked = true;
         }
 
         private void xToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-            SetScale(1.75f);
+            SetScale(1.6f);
             ((ToolStripMenuItem)sender).Checked = true;
         }
 
