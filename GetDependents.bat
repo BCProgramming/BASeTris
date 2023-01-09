adb6271ca81aa033c7432a747fc58a531d8eaff6
BC_Programming <bc_programming@hotmail.com>
2023-01-09 15:05:44 -0800

    update script for retrieving dependent projects to parent directory

--- GetDependents.bat
+++ GetDependents.bat
@@ -7,4 +7,8 @@ cd ..
 mkdir XMLSerialization
 cd XMLSerialization
 git init
-git pull https://github.com/BCProgramming/XMLSerialization
+git pull https://github.com/BCProgramming/Elementizer
+mkdir BCRenderingAdapter
+cd BCRenderingAdapter
+git init
+git pull https://github.com/BCProgramming/BCRendering
